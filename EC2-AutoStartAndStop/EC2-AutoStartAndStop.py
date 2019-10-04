02fa7927f4377634c817f028ea07f9d05e1f72d4
seochan2 <seochan2@gmail.com>
2019-10-04 09:49:01 +0000

    add RDS auto start and stop

--- EC2-AutoStartAndStop/EC2-AutoStartAndStop.py
+++ EC2-AutoStartAndStop/EC2-AutoStartAndStop.py
@@ -2,6 +2,7 @@ import boto3
 region = 'ap-northeast-2'
 # EC2 Instance ID in list format
 targetEC2InstanceIds = ['','']
+targetRDSInstanceIdentifier = ''
 
 def lambda_handler(event, context):
     
@@ -9,10 +10,13 @@ def lambda_handler(event, context):
     targetInstanceState = ec2Resource.Instance(targetEC2InstanceIds[0]).state['Name']
     
     ec2Client = boto3.client('ec2', region_name=region)
+    rdsClient = boto3.client('rds', region_name=region)
     
     if targetInstanceState == 'running':
         ec2Client.stop_instances(InstanceIds=targetEC2InstanceIds)
+        rdsClient.stop_db_instance(DBInstanceIdentifier=targetRDSInstanceIdentifier)
         print "EC2 is Stopping..."
     else:
         ec2Client.start_instances(InstanceIds=targetEC2InstanceIds)
+        rdsClient.stop_db_instance(DBInstanceIdentifier=targetRDSInstanceIdentifier)
         print "EC2 is starting..."
