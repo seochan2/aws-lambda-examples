ceacfefcf81bd104c793321bdb51115b33286316
seochan2 <seochan2@gmail.com>
2019-10-07 12:23:00 +0000

    add datetime for check if it's a weekends

--- EC2-AutoStartAndStop/EC2-AutoStartAndStop.py
+++ EC2-AutoStartAndStop/EC2-AutoStartAndStop.py
@@ -1,11 +1,20 @@
 import boto3
 import time
+from datetime import datetime, timedelta 
+
 region = 'ap-northeast-2'
 # EC2 Instance ID in list format
 targetEC2InstanceIds = ['','']
 targetRDSInstanceIdentifier = ''
 
 def lambda_handler(event, context):
+    
+    seoulDT = datetime.today() + timedelta(hours=9)
+    weekStr = seoulDT.strftime("%a")
+    
+    if weekStr in ['Sat', 'Sun']:
+        print("don't work on weekends")
+        return 0
     
     ec2Resource = boto3.resource('ec2')
     targetInstanceState = ec2Resource.Instance(targetEC2InstanceIds[0]).state['Name']
