91e7377a3bc1f6093f8f60cd5157306ccc0bf531
seochan2 <seochan2@gmail.com>
2020-02-12 11:55:29 +0000

    add chrome options for headless

--- web-test-bot/web-test-bot.py
+++ web-test-bot/web-test-bot.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
 
 def lambda_handler(event, context)
-
-    driver = webdriver.chrome('./chromedriver')
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('windows-size=1920x1080')
+    options.add_argument('disable-gpu')
+    
+    driver = webdriver.chrome('./chromedriver', options=options)
 
     driver.get('https://www.naver.com')
     driver.implicitly_wait(3)
